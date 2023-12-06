0637e94588d3aae646d10365ddafbeed89769895
kmadhuraj <madhurajmadu693@gmail.com>
2023-12-06 17:23:38 +0530

    last commit all changes done ready to go

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-  return (
-    <>
-    
-    </>
-  )
+    return (
+        <>
+            <footer>
+            <div className="questions">Questions? Call 000-800-919-1694</div>
+                <div className="footer">
+                    
+                    <div className="footer-item">
+                        <a href="">FAQ</a>
+                        <a href="">Investor Relations</a>
+                        <a href="">Privacy</a>
+                        <a href="">Speed Test</a>
+                    </div>
+
+                    <div className="footer-item">
+                        <a href="">Help Centre</a>
+                        <a href="">Jobs</a>
+                        <a href="">Cookie Preferences</a>
+                        <a href="">Legal Notices</a>
+                    </div>
+                    <div className="footer-item">
+                        <a href="">Account</a>
+                        <a href="">Ways to Watch</a>
+                        <a href="">Corporate Information</a>
+                        <a href="">Only on Netflix</a>
+                    </div>
+                    <div className="footer-item">
+                        <a href="">Media Centre</a>
+                        <a href="">Terms of Use</a>
+                        <a href="">Contact Us</a>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
 }
